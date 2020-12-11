6b5317be7b5e77933859d4b397b4ea608dde12e3
Arungi Cahaya <47318515+a7cha@users.noreply.github.com>
2020-12-11 14:49:21 +0700

    Update env.js

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
-export const getIp = '54.86.225.191'
+export const getIp = ${{secret.IPAWS}}
 export const API_URI = `http://${getIp}:7000`
 export const REACT_APP_API=`http://${getIp}:7000/zwallet/api/v1`
 export const IMAGE_URI=`http://${getIp}:7000/images/`
